ccbc28fa1d4581ebb4327cfc8d6312d9c3cb2a55
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-09-23 00:51:46 +0500

    empty

--- src/main/java/ru/job4j/set/SimpleArraySet.java
+++ src/main/java/ru/job4j/set/SimpleArraySet.java
@@ -5,32 +5,5 @@ import ru.job4j.collection.SimpleArrayList;
 import java.util.Iterator;
 import java.util.Objects;
 
-public class SimpleArraySet<T> implements SimpleSet<T> {
-
-    private SimpleArrayList<T> set = new SimpleArrayList<>(0);
-
-    @Override
-    public boolean add(T value) {
-        if (!contains(value)) {
-            set.add(value);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean contains(T value) {
-        Iterator<T> iterator = iterator();
-        while (iterator.hasNext()) {
-            if (Objects.equals(iterator.next(), value)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return set.iterator();
-    }
+public class SimpleArraySet {
 }
