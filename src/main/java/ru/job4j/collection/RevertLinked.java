fc78a3c10225b9238819ba11c1aa1e7091b25c26
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-09-23 12:43:17 +0500

    6. Перевернуть связанный список [#161 #514092] (Исправление)

--- src/main/java/ru/job4j/collection/RevertLinked.java
+++ src/main/java/ru/job4j/collection/RevertLinked.java
@@ -10,19 +10,18 @@ public class RevertLinked<T> implements Iterable<T> {
         Node<T> node = new Node<T>(value, null);
         if (head == null) {
             head = node;
-            return;
+        } else {
+            Node<T> tail = head;
+            while (tail.next != null) {
+                tail = tail.next;
+            }
+            tail.next = node;
         }
-        Node<T> tail = head;
-        while (tail.next != null) {
-            tail = tail.next;
-        }
-        tail.next = node;
     }
 
     public boolean revert() {
-        if (head == null || head.next == null) {
-            return false;
-        } else {
+        boolean res = false;
+        if (head != null && head.next != null) {
             Node<T> prevNode = null;
             Node<T> currentNode = head;
             while (currentNode != null) {
@@ -32,8 +31,9 @@ public class RevertLinked<T> implements Iterable<T> {
                 currentNode = nextNode;
             }
             head = prevNode;
-            return true;
+            res = true;
         }
+        return res;
     }
 
     @Override
