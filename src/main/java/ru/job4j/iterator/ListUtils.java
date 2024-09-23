2ee6bdaaa0174275ace10e5ba1aa13356a4edb1b
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-09-23 12:38:46 +0500

    7. ListIterator [#350217 #514097] (Исправление)

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -7,26 +7,14 @@ public class ListUtils {
 
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (iterator.nextIndex() == index) {
-                iterator.add(value);
-                break;
-            }
-            iterator.next();
-        }
+        ListIterator<T> iterator = list.listIterator(index);
+        iterator.add(value);
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (iterator.previousIndex() == index) {
-                iterator.add(value);
-                break;
-            }
-            iterator.next();
-        }
+        ListIterator<T> iterator = list.listIterator(index + 1);
+        iterator.add(value);
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
@@ -48,15 +36,10 @@ public class ListUtils {
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        ListIterator<T> listIterator = list.listIterator();
-        while (listIterator.hasNext()) {
-            T listElemValue = listIterator.next();
-            ListIterator<T> elementsIterator = elements.listIterator();
-            while (elementsIterator.hasNext()) {
-                if (Objects.equals(listElemValue, elementsIterator.next())) {
-                    listIterator.remove();
-                }
-            }
+        ListIterator<T> iterator = elements.listIterator();
+        while (iterator.hasNext()) {
+            T value = iterator.next();
+            removeIf(list, (listElem) -> Objects.equals(listElem, value));
         }
     }
 }
