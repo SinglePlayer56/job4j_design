c6efaafd439f2b3616414bb4902fd678dd2e0e12
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-10-28 15:44:24 +0500

    6. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("Petr Arsentev");
+    }
+
+    @Test
+    void whenFileWithCommentsAndEmptyLines() {
+        String path = "./data/file_with_comments.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name")).isEqualTo("John Doe");
+        assertThat(config.value("city")).isEqualTo("New York");
+        assertThat(config.value("# This is a comment")).isNull();
+        assertThat(config.value("")).isNull();
+    }
+
+    @Test
+    void whenFileWithMalformedLinesThenThrowException() {
+        String path = "./data/malformed_lines.properties";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenValueContainsEqualsSign() {
+        String path = "./data/value_with_equals_sign.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("key")).isEqualTo("value=1");
+    }
+}
