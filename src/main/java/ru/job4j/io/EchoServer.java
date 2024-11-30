b0477b2807aa61a6783fb5dc73e770815cdf31f5
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-11-30 22:47:40 +0500

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -12,10 +12,16 @@ public class EchoServer {
                 try (OutputStream output = socket.getOutputStream();
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
+                    boolean firstLine = true;
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
-                        if (string.equals("GET /?msg=Bye HTTP/1.1")) {
-                            server.close();
+                        if (firstLine) {
+                            switch (string) {
+                                case "GET /?msg=Hello HTTP/1.1" -> output.write("Hello".getBytes());
+                                case "GET /?msg=Exit HTTP/1.1" -> server.close();
+                                default -> output.write("What".getBytes());
+                            }
+                            firstLine = false;
                         }
                         System.out.println(string);
                     }
