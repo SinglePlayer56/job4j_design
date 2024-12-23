18d5226ff824120958263886ad94074e1f4df24a
Viktor Korchagin <viktor.korchagin.corp@gmail.com>
2024-12-24 01:04:07 +0500

    2. Что такое Socket? [#4850 #524072]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,16 +1,15 @@
 package ru.job4j.io;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
 
 public class EchoServer {
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -21,10 +20,9 @@ public class EchoServer {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
                         if (firstLine) {
-                            switch (string) {
-                                case "GET /?msg=Hello HTTP/1.1" -> output.write("Hello".getBytes());
-                                case "GET /?msg=Exit HTTP/1.1" -> server.close();
-                                default -> output.write("What".getBytes());
+                            String msg = extractParamValue(string, "msg");
+                            if (Objects.equals("Bye", msg)) {
+                                server.close();
                             }
                             firstLine = false;
                         }
@@ -33,8 +31,20 @@ public class EchoServer {
                     output.flush();
                 }
             }
-        } catch (IOException e) {
-            LOG.error("Error", e);
         }
+    }
+
+    private static String extractParamValue(String string, String paramName) {
+        String[] parts = string.split(" ");
+        String[] params = parts[1].substring(2).split("&");
+        String result = null;
+        for (String param : params) {
+            String[] keyValue = param.split("=");
+            if (keyValue.length == 2 && keyValue[0].equals(paramName)) {
+                result = keyValue[1];
+                break;
+            }
+        }
+        return result;
     }
 }
